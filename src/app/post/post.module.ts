1ec8dbcce9570e415cce655cb98ea3f0e59cb14d
amanitek <amenychpro@gmail.com>
2024-05-14 14:49:22 +0100

    fix edit and view

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IndexComponent } from './index/index.component';
-import { ViewComponent } from './view/view.component';
-import { CreateComponent } from './create/create.component';
-import { EditComponent } from './edit/edit.component';
+
 
 
 
 @NgModule({
   declarations: [
-  
   ],
   imports: [
     CommonModule
